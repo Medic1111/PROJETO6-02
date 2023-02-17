85bd7ad1626c75716a322a07d85e7b62527f6dc9
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-17 14:50:08 -0300

    colocando o provedor de dados de user na aplicacao toda

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme/theme";
 import App from "./App";
+import { ProvideUser } from "./contexts/users";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <App />
+        <ProvideUser>
+          <App />
+        </ProvideUser>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
