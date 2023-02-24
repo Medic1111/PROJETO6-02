ad8ff7e36cbf92478fb4efac46fe2e022698c728
Aryse Tansy <medictansy@gmail.com>
2023-02-23 20:28:46 -0500

    Micro: Seed DB, Model Products, Mock Data

---
+++ server/db/seed.js
@@ -0,0 +1,70 @@
+const mockData = [
+  {
+    title: "test one",
+    price: 123,
+    description: "testing fkeoakfoakofkaokfoaskfokasofk",
+    url: "testing",
+    stock_count: 89,
+  },
+  {
+    title: "test one",
+    price: 123,
+    description: "testing fkeoakfoakofkaokfoaskfokasofk",
+    url: "testing",
+    stock_count: 89,
+  },
+  {
+    title: "test one",
+    price: 123,
+    description: "testing fkeoakfoakofkaokfoaskfokasofk",
+    url: "testing",
+    stock_count: 89,
+  },
+];
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { Product } = require("../models/products");
+
+const connection = async () => {
+  await mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+      console.log("db pronta para popular!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const seedDB = async (Product, mockData) => {
+  await connection();
+
+  await Product.create(mockData)
+    .then(() => {
+      console.log("DB POPULADA");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  process.exit();
+};
+
+const clearDB = async (Product) => {
+  await connection();
+
+  await Product.deleteMany()
+    .then(() => {
+      console.log("DB CLEARED");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  process.exit();
+};
+
+if (process.argv[2] === "--seedDB") {
+  seedDB(Product, mockData);
+} else if (process.argv[2] === "--clearDB") {
+  clearDB(Product, mockData);
+}
