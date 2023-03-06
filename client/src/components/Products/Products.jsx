f9e54cd8e198e54d900e739efeeca04d053b5e7c
ANDRESSA CHAGAS <andressa.chagas79@gmail.com>
2023-03-06 20:45:48 -0300

    Resolucao de erro, mais implementacoes do metodo S.O.L.I.D

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,10 +1,11 @@
 import style from "./Products.module.css"
 import { useFetchApi } from "../../hooks/useFetchApi/useFetchApi";
-import {Nav} from "../ProductsComps/Nav/Nav";
+import { Nav } from "../ProductsComps/Nav/Nav";
 import { useState } from "react";
+import { ProductsList } from "../ProductsComps/ProductsList/ProductList";
 
 
-const Products = () => {
+export const Products = () => {
 
 const [url, setUrl] = useState("/api/v1/products")
 
@@ -13,9 +14,9 @@ const products = useFetchApi(url)
 
    <div className={style.center}> 
 	  <Nav setUrl={setUrl} />
+	  <ProductsList products={products} />
 		
 </div> 
    )
 }
 
-export default Products
